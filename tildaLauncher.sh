7b5f95f1e66f5699b8c1a61844236ef92ab8f57f
Pierce <gripwolf@gmail.com>
2018-02-03 15:29:45 -0600

    Initial commit

--- tildaLauncher.sh
+++ tildaLauncher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$(pidof tilda)" != "" ]; then
-    xdotool key ctrl+alt+x
+if [ "$(pidof tilda)" != "" ]; then #Checks if tilda is already running
+    xdotool key ctrl+alt+x #Replace ctrl+alt+x with your configured binding for "Pull down terminal"
 else
-    /usr/bin/tilda &
+    tilda & #Forks tilda to background, allowing this script's process to terminate
 fi
